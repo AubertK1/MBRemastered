e7216df856c7ca07826fe6decb4d1da0bad09b7f
AubertK1 <aubertjkamdem@gmail.com>
2022-10-03 13:57:56 -0400

    Start

---
+++ core/src/com/mygdx/project/Main.java
@@ -0,0 +1,32 @@
+package com.mygdx.project;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Main extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	int arm;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 050, 0100);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ desktop/src/com/mygdx/project/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.mygdx.project;
+
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.project.Main;
+
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Frame2");
+		config.setMaximized(true);
+		new Lwjgl3Application(new Main(), config);
+	}
+}
